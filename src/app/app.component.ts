5b4249a0d017f8dcc3d898709bf08fbd71e2db69
rahmman <rahmman.cse@outlook.com>
2017-02-26 11:49:44 -0600

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
-
+import{FormControl, FormGroup, Validators} from '@angular/forms'
+import{User} from './user.interface'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+user :FormGroup;
+constructor() {}
+ngOnInit(){
+this.user=new FormGroup({
+name: new FormControl('',[Validators.required, Validators.minLength(2)]),
+account: new FormGroup({
+  email: new FormControl('', Validators.required),
+  confirm: new FormControl('', Validators.required)
+})
+})
+
+}
+onSubmit({value, valid} : {value:User, valid: boolean}){
+  console.log(value, valid);
+}
+
 }
